fa2aaab4e1aafa4640854466ad4ab8d4423394d4
Nazar Zozulya <115177222+Nazar-Zozulya@users.noreply.github.com>
2024-09-10 21:06:43 +0300

    Добавил скролл в навигации

---
+++ HOME_CAKE/mainapp/static/main_app/js/main.js
@@ -0,0 +1,25 @@
+
+
+// Подключаю кнопки навигации
+var headerMain = document.querySelector('#headerMain')
+var headerMain = document.querySelector('#headerNews')
+var headerMain = document.querySelector('#headerAboutUs')
+var headerMain = document.querySelector('#headerAssortment')
+
+// Делаю скролл при нажатии
+headerMain.onclick = function () {
+    window.scrollTo(0, 0)
+}
+
+headerNews.onclick = function () {
+    document.querySelector('.new').scrollIntoView()
+}
+
+headerAboutUs.onclick = function () {
+    document.querySelector('.about-us h1').scrollIntoView()
+}
+
+headerAssortment.onclick = function () {
+    document.querySelector('.assortment').scrollIntoView()
+}
+
